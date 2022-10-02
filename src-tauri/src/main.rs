4dcb65fe374c8c91e28b694a18e10a573c0fb855
Mario Cabral <mariocabral.dev@gmail.com>
2022-10-02 17:41:07 -0300

    rocket integration

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,30 @@
     windows_subsystem = "windows"
 )]
 
+
+#[rocket::get("/hello")]
+fn world() -> &'static str {
+  "Hello, world!"
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|_app| {
+            tauri::async_runtime::spawn(
+              rocket::build()
+                    .mount("/hello", rocket::routes![world])
+                    .launch()
+            );
+            Ok(())
+          })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
