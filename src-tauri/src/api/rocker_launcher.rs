d578cb7c04569ddaf785b14b87d896632ce179a6
Mario Cabral <mariocabral.dev@gmail.com>
2022-10-10 14:00:57 -0300

    add CORS support

--- src-tauri/src/api/rocker_launcher.rs
+++ src-tauri/src/api/rocker_launcher.rs
@@ -1,6 +1,8 @@
 extern crate rocket;
 extern crate dotenv;
-use rocket::{Request, Rocket, catch, catchers, routes, Build};
+use rocket::http::Header;
+use rocket::{Request, Response, Rocket, catch, catchers, routes, Build};
+use rocket::fairing::{Fairing, Info, Kind};
 use crate::repository::mongodb_config;
 use crate::api::profesional_api;
 use rocket_db_pools::{mongodb, Database};
@@ -19,6 +21,24 @@ fn not_found(req: &Request) -> String {
     format!("I couldn't find '{}'. Try something else?", req.uri())
 }
 
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 pub fn rocket() -> Rocket<Build> {
     let figment = rocket::Config::figment()
@@ -35,4 +55,5 @@ pub fn rocket() -> Rocket<Build> {
                     profesional_api::put,
                     profesional_api::delete,]
         )
+        .attach(CORS)
 }
